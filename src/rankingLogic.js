f9aa9d26b3c1dd33b249389e44fb8e1e3dc5bf92
b50880423-hub <b50880423@gmail.com>
2026-08-08 15:55:28 +0530

    Update rankingLogic.js

--- src/rankingLogic.js
+++ src/rankingLogic.js
@@ -20,7 +20,7 @@ function normalizeUsername(value = '') {
 function buildUserLink(user) {
   const rawName = normalizeDisplayName(user.displayName || user.userName || `User ${user.userId}`);
   const cleanName = rawName.replace(/[\uD800-\uDFFF]/g, '');
-  const shortName = rawName.length > 14 ? `${rawName.slice(0, 14)}...` : rawName;
+  const shortName = rawName.length > 28 ? `${rawName.slice(0, 28)}...` : rawName;
   const name = escapeHtml(shortName);
   const userId = user.userId;
   const username = normalizeUsername(user.userName);
